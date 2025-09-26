3ee6f59b79ce790ef47d6cdf63f4a8dc10a45f1f
willweaver <me@willweaver.dev>
2025-09-26 10:19:33 -0400

    Add docker aliases and functions

--- home.nix
+++ home.nix
@@ -8,12 +8,26 @@
     docker
   ];
 
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      drm() { docker rm $(docker ps -q -a); }
+      dri() { docker rmi $(docker images -q); }
+      db() { docker build -t="$1" .; }
+      dps() { docker ps -a; }
+      eval "$(direnv hook zsh)"
+    '';
+  };
+
   home.shellAliases = {
     g = "git";
     "..." = "cd ../..";
+    dip = "docker inspect --format '{{ .NetworkSettings.IPAddress }}'";
+    dkd = "docker run -d -P";
+    dki = "docker run -t -i -P";
   };
-
-  programs.home-manager.enable = true;
 
   programs.git = {
     enable = true;
