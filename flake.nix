7f15d647e18a0b9a283535bfe07b705b39a439c7
Will Weaver <monkey@buildingbananas.com>
2025-10-14 18:56:45 -0400

    Create separate linux-home for debugging

--- flake.nix
+++ flake.nix
@@ -118,7 +118,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.monkey = import ./home.nix;
+          home-manager.users.monkey = import ./linux-home.nix;
         }
       ];
     };
