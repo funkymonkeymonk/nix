377ea9eaefa53cad6acd91cea4bab2f774e7388a
Will Weaver <monkey@buildingbananas.com>
2025-10-14 18:35:32 -0400

    Add home manager

--- flake.nix
+++ flake.nix
@@ -114,6 +114,12 @@
         }
         configuration
         ./minimal.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.monkey = import ./home.nix;
+        }
       ];
     };
   };
