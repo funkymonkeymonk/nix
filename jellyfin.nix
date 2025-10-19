249d486bbd90e3a9a113e2ba66a025801212489f
Will Weaver <monkey@buildingbananas.com>
2025-10-19 03:45:51 -0400

    Open firewall for Jellyfin

--- jellyfin.nix
+++ jellyfin.nix
@@ -6,6 +6,7 @@
   services.jellyfin = {
     enable = true;
     package = pkgs.unstable.jellyfin;
+    services.jellyfin.openFirewall = true;
     # Create a Jellyfin user and run as that user
     # user = "jellyfin";
   };
