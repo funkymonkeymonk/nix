765a4e64762eab7f30319bcdb8dabbe208c376bc
Will Weaver <monkey@buildingbananas.com>
2025-10-29 22:39:38 -0400

    feat(home): add zsh configuration with initContent to home.nix
    - Add docker functions (drm, dri, db, dps) to zsh init
    - Add direnv hook integration
    - Add macOS-specific iso2usb function
    - Enable home-manager program

--- home.nix
+++ home.nix
@@ -46,6 +46,48 @@
 
   # Consolidated `programs` attribute set
   programs = {
+    "home-manager".enable = true;
+
+    zsh = {
+      enable = true;
+      initContent = ''
+        # Docker
+        drm() { docker rm $(docker ps -q -a); }
+        dri() { docker rmi $(docker images -q); }
+        db() { docker build -t="$1" .; }
+        dps() { docker ps -a; }
+
+        # Direnv
+        eval "$(direnv hook zsh)"
+
+        # ISO to USB function (macOS specific)
+        ${lib.optionalString pkgs.stdenv.isDarwin ''
+          iso2usb() {
+            if [ -f "$1" ]; then
+              iso_name=$1
+              hdiutil convert -format UDRW -o ./temp.img $iso_name
+              mv temp.img.dmg temp.img
+              diskutil list
+              echo "** Be careful. **"
+              echo "This will wipe all data on the disk."
+              echo "Which disk number would you like to install to:"
+              read disk_num
+
+              if [ -b "/dev/disk$disk_num" ]; then
+                diskutil unmountDisk /dev/disk$disk_num
+                sudo dd if=./temp.img of=/dev/rdisk$disk_num bs=1m
+              else
+                echo Disk $disk_num is not found.
+              fi
+              rm temp.img
+            else
+              echo "Usage: iso2usb iso_file.iso"
+            fi
+          }
+        ''}
+      '';
+    };
+
     alacritty = {
       enable = true;
       settings = {
