ac53a95a05a472261fe1e79be14cd0f840a9e930
Will Weaver <monkey@buildingbananas.com>
2025-10-29 23:59:58 -0400

    refactor(darwin): consolidate macOS packages into platform bundle
    - Move all packages from os/darwin.nix to bundles/platforms/darwin/default.nix
    - Remove duplicate environment.systemPackages from os/darwin.nix
    - Keep only core system settings (nix, sudo, defaults) in os/darwin.nix
    - Eliminates duplication and follows intended modular architecture

--- bundles/platforms/darwin/default.nix
+++ bundles/platforms/darwin/default.nix
@@ -10,6 +10,23 @@
 
     # Development tools with macOS support
     colima # Docker alternative for macOS
+
+    # Additional system packages
+    trippy
+    logseq
+    ripgrep
+    fd
+    coreutils
+    clang
+    slack
+    home-manager
+    glow
+    antigen
+    k3d
+    kubectl
+    kubernetes-helm
+    k9s
+    the-unarchiver
   ];
 
   # 1Password integration
--- os/darwin.nix
+++ os/darwin.nix
@@ -16,29 +16,4 @@
       autohide = true;
     };
   };
-
-  environment.systemPackages = with pkgs; [
-    google-chrome
-    trippy
-    logseq
-    ripgrep
-    fd
-    coreutils
-    clang
-    slack
-    home-manager
-    colima
-    hidden-bar
-    glow
-    goose-cli
-    antigen
-    alacritty-theme
-    #atuin - check this out later
-    claude-code
-    k3d
-    kubectl
-    kubernetes-helm
-    k9s
-    the-unarchiver
-  ];
 }
