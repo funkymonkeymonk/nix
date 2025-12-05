09cced02459a8e4b9dd7033745797e21bf352f1a
Will Weaver <me@willweaver.dev>
2025-12-04 20:46:56 -0500

    feat: Add moonlight-qt to gaming role bundle

--- bundles/roles/gaming/default.nix
+++ bundles/roles/gaming/default.nix
@@ -6,6 +6,7 @@
 }: {
   # Gaming role bundle - tools for gaming and entertainment
   environment.systemPackages = with pkgs; [
+    moonlight-qt  # Game streaming client for playing games from remote hosts
     # Gaming platforms will be added here
     # Note: Most gaming applications are platform-specific and handled in platform bundles
   ];
