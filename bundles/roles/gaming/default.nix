bcfefb1b0bfd3af400490b524fdc1d6caa0aa54a
Will Weaver <me@willweaver.dev>
2025-12-11 22:02:18 -0500

    Add moonlight game streaming

--- bundles/roles/gaming/default.nix
+++ bundles/roles/gaming/default.nix
@@ -6,7 +6,8 @@
 }: {
   # Gaming role bundle - tools for gaming and entertainment
   environment.systemPackages = with pkgs; [
+    moonlight-qt
     # Gaming platforms will be added here
-    # Note: Most gaming applications are platform-specific and handled in platform bundles
+    # Note: Most gaming applications are platforam-specific and handled in platform bundles
   ];
 }
