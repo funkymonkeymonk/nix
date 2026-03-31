12ed9cfa67f928d172fc016ca8d317105eee3243
Will Weaver <me@willweaver.dev>
2026-03-31 16:54:40 -0400

    Rm jj workspace (#195)
    * Remove workspaces
    
    * update gitignore
    
    ---------
    
    Co-authored-by: Will Weaver <monkey@megamanx.home.buildingbananas.com>

--- workspaces/nix-openclaw/overlays/default.nix
+++
@@ -1,6 +0,0 @@
-# Custom package overlays
-final: _prev: {
-  rtk = final.callPackage ../packages/rtk {};
-  yaks = final.callPackage ../packages/yaks {};
-  pi-coding-agent = final.callPackage ../packages/pi-coding-agent {};
-}
