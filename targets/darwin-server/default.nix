3c1087971acb776dec2712ca77eb4f9187cd4b57
Will Weaver <me@willweaver.dev>
2026-05-18 14:46:58 -0400

    fix: use allowInsecurePredicate to permit all openclaw versions

--- targets/darwin-server/default.nix
+++ targets/darwin-server/default.nix
@@ -10,9 +10,11 @@
   ...
 }: {
   nixpkgs.hostPlatform = "aarch64-darwin";
-  nixpkgs.config.permittedInsecurePackages = [
-    "openclaw-2026.4.22"
-  ];
+  nixpkgs.config.allowInsecurePredicate = attrs:
+    let pname = attrs.pname or attrs.name or "";
+        fullName = "${pname}-${attrs.version or ""}";
+    in pname == "openclaw"
+       || builtins.elem fullName ["olm-3.2.16"];
   system.stateVersion = 4;
   system.primaryUser = "monkey";
 
