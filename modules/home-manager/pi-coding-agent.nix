486b4b2df9847de17ee14b1b5c86e93bdb925bb1
Will Weaver <me@willweaver.dev>
2026-06-24 10:05:08 -0400

    chore: move stray yak file to .yaks directory

--- modules/home-manager/pi-coding-agent.nix
+++ modules/home-manager/pi-coding-agent.nix
@@ -224,7 +224,6 @@ with lib; let
                 input: ["text"],
                 cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
                 contextWindow: 128000,
-                maxTokens: 4096,
               }));
               pi.registerProvider("bifrost", {
                 baseUrl: "${bifrostBase}",
